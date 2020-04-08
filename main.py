7f57679745cfe9804c4983d9f5fa781054d0f2d8
Shagun Bandi <shagunbandi@dev.azure.com>
2020-04-09 00:59:22 +0530

    main file

---
+++ main.py
@@ -0,0 +1,29 @@
+import fileinput, re
+import sys
+
+
+path="/Users/shabandi/Desktop/All Photos"
+
+
+def modify_file(value):
+    print("Here")
+    file_name = "/Users/shabandi/Projects/Projects/Personal/AlbumHere/DjangoServer/DjangoServer/settings.py"
+    fh = fileinput.input(file_name, inplace=True)
+    for line in fh:
+        print(line)
+        replacement = "MEDIA_ROOT=\"" + value + "\""
+        line = re.sub('MEDIA_ROOT=.+', replacement, line)
+        sys.stdout.write(line)
+    fh.close()
+
+
+modify_file(path)
+
+#
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoServer.settings")
+#
+# from django.core.management import call_command
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+# call_command('runserver',  '127.0.0.1:8000')

ef90411fb2923ad4650dd9b32f73df4dc3ec4ba8
Shagun Bandi <shagunbandi@dev.azure.com>
2020-04-09 01:02:15 +0530

    file name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import fileinput, re
 import sys
+import os
+from os.path import isfile, join
 
 
 path="/Users/shabandi/Desktop/All Photos"
@@ -7,10 +9,9 @@ path="/Users/shabandi/Desktop/All Photos"
 
 def modify_file(value):
     print("Here")
-    file_name = "/Users/shabandi/Projects/Projects/Personal/AlbumHere/DjangoServer/DjangoServer/settings.py"
+    file_name = join(os.getcwd(), 'DjangoServer/settings.py')
     fh = fileinput.input(file_name, inplace=True)
     for line in fh:
-        print(line)
         replacement = "MEDIA_ROOT=\"" + value + "\""
         line = re.sub('MEDIA_ROOT=.+', replacement, line)
         sys.stdout.write(line)
@@ -20,7 +21,7 @@ def modify_file(value):
 modify_file(path)
 
 #
-# import os
+
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoServer.settings")
 #
 # from django.core.management import call_command

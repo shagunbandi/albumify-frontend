a4de063be67f38f5fed4d47128344a34b7b8ca66
Shagun Bandi <shagunbandi@dev.azure.com>
2020-06-26 16:54:40 +0530

    Updates

---
+++ precache-manifest.27ae71359800f07f27c40af514cc5e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/albumify-frontend/static/media/loading1.3f259006.gif"
+  },
+  {
+    "revision": "3135450284db17f531ea",
+    "url": "/albumify-frontend/static/js/runtime~main.31354502.js"
+  },
+  {
+    "revision": "e296336a87b34eb29c4d",
+    "url": "/albumify-frontend/static/js/main.e296336a.chunk.js"
+  },
+  {
+    "revision": "330370db38deb00eb815",
+    "url": "/albumify-frontend/static/js/1.330370db.chunk.js"
+  },
+  {
+    "revision": "e296336a87b34eb29c4d",
+    "url": "/albumify-frontend/static/css/main.ecda9776.chunk.css"
+  },
+  {
+    "revision": "9321724b0ec86407ad20c40281657240",
+    "url": "/albumify-frontend/index.html"
+  }
+];

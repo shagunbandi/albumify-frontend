d993713b9c658b471347db716e26461aa91def4b
Shagun Bandi <shagunbandi@dev.azure.com>
2020-06-30 22:29:17 +0530

    Info Page

--- src/components/instructions/index.js
+++ src/components/instructions/index.js
@@ -4,8 +4,10 @@ export class Instructions extends Component {
 
     render() {
         return (
-            <div className="pd-4">
-                Is your Django Server Running. Run the Django Server and refresh
+            <div className="pd-4 text-white info-div">
+                <br/>
+                <h3>Is your Django Server Running ?? Run the Django Server and refresh</h3><br /><br />
+                <h4>For help <a target="_blank" rel="noopener noreferrer" href="https://github.com/shagunbandi/albumify-backend/blob/master/README.md">Click Here</a></h4>
             </div>
         );
     }
